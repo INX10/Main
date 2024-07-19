108f6b44c9d91b43de3ff1a4ed00dc42425c9b5b
Ryanmarkluis <91365050+Ryanmarkluis@users.noreply.github.com>
2024-07-19 19:30:31 +0800

    admin_page

---
+++ inx10/resources/views/admin.blade.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to INX10!</title>
+    <link rel="icon" href="{{ URL('images/logo_title.png') }}" type="image/png">
+</head>
+<body>
+    
+</body>
+</html>
